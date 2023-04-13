8f007c814fa8df0095bc7d9027b72a85cdf7c007
Jan Youssef <janyoussef19@gmail.com>
2023-04-13 10:12:32 +0200

    refactor

--- Tasks/src/main/java/streams/Main.java
+++ Tasks/src/main/java/streams/Main.java
@@ -1,13 +1,13 @@
 package streams;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
 
     private static final List<City> cities = Repository.getCities();
     private static final List<Country> countries = Repository.getCountries();
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
 
 
     }
