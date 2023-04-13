dfbfeffbed1b4c2b756bfd2c5901164244743cca
Jan Youssef <janyoussef19@gmail.com>
2023-04-13 10:11:49 +0200

    refactor

--- Tasks/src/main/java/streams/Main.java
+++ Tasks/src/main/java/streams/Main.java
@@ -2,18 +2,14 @@ package streams;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
+    private static final List<City> cities = Repository.getCities();
+    private static final List<Country> countries = Repository.getCountries();
     public static void main(String[] args) throws IOException {
-        List<String> cities = Repository.getAsList("/Cities.csv");
-        List<String> countries = Repository.getAsList("/Countries.csv");
 
-        List<Country> cityList = countries.stream()
-                .map(Country::fromString)
-                .collect(Collectors.toList());
-        cityList.forEach(System.out::println);
+
     }
 
 }
--- Tasks/src/main/java/streams/Repository.java
+++ Tasks/src/main/java/streams/Repository.java
@@ -7,16 +7,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Repository {
-    static final String CURRENT_PATH = Paths.get("")
+    private static final String CURRENT_PATH = Paths.get("")
             .toAbsolutePath() + "/Tasks/src/main/java/streams/resources";
 
-    static List<String> getAsList(String x) throws IOException {
-        return Files.readAllLines(Paths.get(CURRENT_PATH, x))
-                .stream()
-                .map(String::trim)
+    private static List<String> getAsList(String file) {
+        try {
+            return Files.readAllLines(Paths.get(CURRENT_PATH, file))
+                    .stream()
+                    .map(String::trim)
+                    .map(l -> l.replace(", ", ","))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                .map(l -> l.replace(", ", ","))
+
+    public static List<City> getCities() {
+        return getAsList("/Cities.csv").stream()
+                .map(City::fromString)
                 .collect(Collectors.toList());
     }
 
+    public static List<Country> getCountries() {
+        return getAsList("/Countries.csv").stream()
+                .map(Country::fromString)
+                .collect(Collectors.toList());
+    }
 }
