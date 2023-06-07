3def5d20b37f4a889328aab813107ebcc853b4ff
Jan Youssef <janyoussef19@gmail.com>
2023-06-08 02:25:21 +0300

    use countdown latches to avoid test cases mixing output

--- Tasks/src/main/java/threads/ticketbooking/Main.java
+++ Tasks/src/main/java/threads/ticketbooking/Main.java
@@ -5,26 +5,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         System.out.println("Running test cases...");
+        System.out.println("First testcase:\n");
         TestCases.ThousandThreads100Seats();
-        AvoidMixedOutput();
+        System.out.println("Second testcase:\n");
         TestCases.SeatPurchasedAlready();
-        AvoidMixedOutput();
+        System.out.println("Third testcase:\n");
         TestCases.EventSoldOut();
-    }
-
-    private static void AvoidMixedOutput() {
-        /*
-         2 threads because main thread and IntelliJ's thread
-         https://stackoverflow.com/questions/30561424/thread-count-is-different-when-debugging-or-running-on-intellij
-        */
-        final int MIN_THREADS = 2;
-        while (Thread.activeCount() > MIN_THREADS) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
 }
--- Tasks/src/main/java/threads/ticketbooking/TestCases.java
+++ Tasks/src/main/java/threads/ticketbooking/TestCases.java
@@ -2,52 +2,78 @@ package threads.ticketbooking;
 
 import threads.ticketbooking.domain.Event;
 
+import java.util.concurrent.CountDownLatch;
+
 public class TestCases {
-    static void ThousandThreads100Seats() {
+    static void ThousandThreads100Seats() throws InterruptedException {
         Event event = new Event(10, 10);
-        for (int i = 0; i < 1000; i++) {
-            new Thread(() -> event.bookSeat(Thread.currentThread()
-                                                    .getName(), (int) (Math.random() * 100))).start();
+        int numThreads = 1000;
+        CountDownLatch latch = new CountDownLatch(numThreads);
+
+        for (int i = 0; i < numThreads; i++) {
+            //using Runnable more readable than lambda for me
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    event.bookSeat(Thread.currentThread().getName(), (int) (Math.random() * 100));
+                    latch.countDown();
+                }
+            });
+            t.start();
+
+
         }
-        printStats("\n\n\n\n================ ThousandThreads100Seats Statistics =================", event,
+        latch.await();
+        printStats("================ ThousandThreads100Seats Statistics =================", event,
                    "=====================================================================");
     }
 
-    static void SeatPurchasedAlready() {
+    static void SeatPurchasedAlready() throws InterruptedException {
         Event event = new Event(10, 10);
         int seatId = 1;
-        new Thread(() -> event.bookSeat(Thread.currentThread()
-                                                .getName(), seatId)).start();
-        new Thread(() -> event.bookSeat(Thread.currentThread()
-                                                .getName(), seatId)).start();
-        printStats("\n\n\n\n================= SeatPurchasedAlready Statistics =================", event,
+        CountDownLatch latch = new CountDownLatch(2);
+
+
+        new Thread(() -> {
+            event.bookSeat(Thread.currentThread()
+                                   .getName(), seatId);
+            latch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            event.bookSeat(Thread.currentThread()
+                                   .getName(), seatId);
+            latch.countDown();
+        }).start();
+
+        latch.await();
+        printStats("================= SeatPurchasedAlready Statistics =================", event,
                    "===================================================================");
     }
 
-    static void EventSoldOut() {
+    static void EventSoldOut() throws InterruptedException {
         Event event = new Event(10, 10);
         event.setSoldOut(true);
+
+        CountDownLatch latch = new CountDownLatch(10);
         for (int i = 0; i < 10; i++) {
-            new Thread(() -> event.bookSeat(Thread.currentThread()
-                                                    .getName(), (int) (Math.random() * 100))).start();
+            new Thread(() -> {
+                event.bookSeat(Thread.currentThread()
+                                       .getName(), (int) (Math.random() * 100));
+                latch.countDown();
+            }).start();
         }
-        printStats("\n\n\n\n====================== EventSoldOut Statistics ======================", event,
+        latch.await();
+        printStats("====================== EventSoldOut Statistics ======================", event,
                    "=====================================================================");
     }
 
     private static void printStats(String top, Event event, String bottom) {
-        waitForTestCaseToFinishExecuting();
-
+        System.out.println();
         System.out.println(top);
         System.out.println(event.getStatistics());
         System.out.println(bottom);
+        System.out.println("\n\n\n\n");
     }
 
-    private static void waitForTestCaseToFinishExecuting() {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-
-        }
-    }
 }
