74bb32aef8121f19d9407d34572146a0d5d0dba0
Jan Youssef <janyoussef19@gmail.com>
2023-06-17 13:31:50 +0300

    finished tasks

---
+++ Tasks/src/main/java/streams2/Main.java
@@ -0,0 +1,14 @@
+package streams2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Running test cases...");
+        TestCases.allBooks();
+        TestCases.allBooksWithPriceAboveHundred();
+        TestCases.discountedToys();
+        TestCases.cheapestBooks(3);
+
+
+    }
+
+}
