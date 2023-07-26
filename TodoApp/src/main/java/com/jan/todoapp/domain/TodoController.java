777a972df8488e1f026bff1b7e4a775ac4ceb4a6
Jan Youssef <janyoussef19@gmail.com>
2023-07-27 00:33:21 +0300

    [feat] Add readAll and better exceptions

--- TodoApp/src/main/java/com/jan/todoapp/domain/TodoController.java
+++ TodoApp/src/main/java/com/jan/todoapp/domain/TodoController.java
@@ -3,31 +3,38 @@ package com.jan.todoapp.domain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/todos")
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoService todoService;
 
+    @GetMapping
+    public List<TodoItem> readAllTodos() {
+        return todoService.readAll();
+    }
+
     @PostMapping
-    public TodoItem create(@RequestBody TodoItem todoItem) {
+    public TodoItem createTodo(@RequestBody TodoItem todoItem) {
         return todoService.createOrUpdate(todoItem);
     }
 
     @GetMapping("/{id}")
-    public TodoItem read(@PathVariable Integer id) {
+    public TodoItem readTodo(@PathVariable Integer id) {
         return todoService.read(id);
     }
 
 
     @RequestMapping(value = "/{id}", method = {RequestMethod.PATCH, RequestMethod.PUT})
-    public TodoItem update(@PathVariable Integer id, @RequestBody TodoItem todoItem) {
+    public TodoItem updateTodo(@PathVariable Integer id, @RequestBody TodoItem todoItem) {
         todoItem.setId(id);
         return todoService.createOrUpdate(todoItem);
     }
 
     @DeleteMapping("/{id}")
-    public TodoItem delete(@PathVariable Integer id) {
-        return todoService.delete(id);
+    public void deleteTodo(@PathVariable Integer id) {
+        todoService.delete(id);
     }
 }
--- TodoApp/src/main/java/com/jan/todoapp/domain/TodoService.java
+++ TodoApp/src/main/java/com/jan/todoapp/domain/TodoService.java
@@ -1,22 +1,33 @@
 package com.jan.todoapp.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepository;
+
+    public List<TodoItem> readAll() {
+        return todoRepository.findAll();
+    }
+
     public TodoItem createOrUpdate(TodoItem todoItem) {
         return todoRepository.save(todoItem);
     }
+
     public TodoItem read(int id) {
-        return todoRepository.findById(id).orElseThrow();
+        return todoRepository.findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                                              "TodoItem with id " + id + "not found"));
     }
-    public TodoItem delete(int id) {
-        TodoItem todoItem = todoRepository.findById(id).orElseThrow();
-        todoRepository.delete(todoItem);
-        return todoItem;
+
+    public void delete(int id) {
+        todoRepository.deleteById(id);
     }
 
 }
